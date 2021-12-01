bd206bf56d9644b519888151ff3e689f6bd54ee0
Aaron Dhiman <aaron.dhiman@gmail.com>
2021-12-01 11:05:50 -0500

    found better way to extract the job ids for the url list

---
+++ README.md
@@ -0,0 +1,5 @@
+This script is great to get all the results in an unfiltered way (e.g., no promoted jobs or other sorting/filtering) and allows results to come to you without creating an indeed account/profile.
+- requires mailx package
+- encapsulate search terms with quotes
+- NOTE: email might go into SPAM, so need to whitelist your server email address
+
--- indeedSearch.sh
+++ indeedSearch.sh
@@ -7,28 +7,36 @@ terms=$1
 echo $1 $2
 
 # search job 
-curl -s --data-urlencode "as_and=$1" -H "referer: https://www.indeed.com/?from=gnav-passport--passport-webapp&_ga=2.31817722.599025073.1618078236-916168758.1617925914" "https://www.indeed.com/jobs?as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=25&l=$2&fromage=any&limit=50&sort=&psf=advsrch&from=advancedsearch" >jobResults.html 
+echo searching for $terms jobs in $zip
+curl -s --data-urlencode "as_and=$terms" -H "referer: https://www.indeed.com/?from=gnav-passport--passport-webapp&_ga=2.31817722.599025073.1618078236-916168758.1617925914" "https://www.indeed.com/jobs?as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=25&l=$zip&fromage=any&limit=50&sort=&psf=advsrch&from=advancedsearch" >jobResults.html 
 grep "data-jk=" jobResults.html > jobIDs.txt
 echo "Unique Jobs = `wc -l jobIDs.txt`"
-numResults=`grep -i "1 of " jobResults.html | cut -d'f' -f2| cut -d' ' -f2|sed s/,//`
+numResults=`grep -i "1 of " jobResults.html | cut -d'f' -f2| cut -d' ' -f2 | sed s/,// | sed /^[[:alpha:]]/d`
+echo numResults = $numResults
 numPages=$((numResults / 50 + 1))
+echo numPages = $numPages
 x=1
 while [ $x -lt $numPages ]
 do 
 	startingJob=$((x*50))
-	#echo startingJob = $startingJob
-	curl -s --data-urlencode "as_and=$1" -H "referer: https://www.indeed.com/?from=gnav-passport--passport-webapp&_ga=2.31817722.599025073.1618078236-916168758.1617925914" "https://www.indeed.com/jobs?as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=25&l=$2&fromage=any&limit=50&start=$startingJob&sort=&psf=advsrch&from=advancedsearch" >jobResults.html
+	echo startingJob = $startingJob
+	curl -s --data-urlencode "as_and=$terms" -H "referer: https://www.indeed.com/?from=gnav-passport--passport-webapp&_ga=2.31817722.599025073.1618078236-916168758.1617925914" "https://www.indeed.com/jobs?as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=25&l=$zip&fromage=any&limit=50&start=$startingJob&sort=&psf=advsrch&from=advancedsearch" >jobResults.html
 	grep "data-jk=" jobResults.html >> jobIDs.txt
-	echo "Unique Jobs = `wc -l jobIDs.txt`"
-	sed -i 's/data-//' jobIDs.txt
+  grep "jobmap" jobResults.html >> results0.json
+  cat results0.json | cut -d '=' -f2 | sed 's/;//g' | sed 's/^ //g' > results1.json
+  cat results1.json | sed 's/,.*//g' | sed 's/{jk:/jk=/' | sed "s/'//g"  > jk.txt
+	echo "Unique Jobs = `wc -l jk.txt`"
 	x=$((x + 1))
 done
-sed -i 's/"//g' jobIDs.txt
-sed -i 's/jk/https:\/\/www.indeed.com\/viewjob?jk/g' jobIDs.txt
-cat jobIDs.txt
-rm jobResults.html
+sed 's/jk=/https:\/\/www.indeed.com\/viewjob?jk=/g' jk.txt > urls.txt
 echo "-----------------------------------"
 echo "number of results = $numResults"
 echo "number of pages = $numPages"
-echo "emailing to $3"
-echo "`cat jobIDs.txt`" | mail -s "Indeed Jobs" $3
+echo "emailing to $email"
+echo "`cat urls.txt`" | mail -s "Indeed Jobs" $email
+rm results*json
+rm jobIDs.txt
+rm jobResults.html
+rm jk.txt
+rm urls.txt
+
